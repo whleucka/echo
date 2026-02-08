bc034583151816d98ae5f7f3f0b44674e044ed16
William Hleucka <william.hleucka@gmail.com>
2026-02-08 07:54:14 -0700

    release: v0.4.8

--- src/Framework/Http/Response.php
+++ src/Framework/Http/Response.php
@@ -85,7 +85,7 @@ class Response implements HttpResponse
         header("X-Content-Type-Options: nosniff");
         header("Referrer-Policy: strict-origin-when-cross-origin");
         header("X-XSS-Protection: 1; mode=block");
-        header("Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; img-src 'self' data: https://www.gravatar.com; font-src 'self' https://cdn.jsdelivr.net; connect-src 'self'; frame-ancestors 'none'; object-src 'none'; base-uri 'self'");
+        header("Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net; style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; img-src 'self' data: https://www.gravatar.com; font-src 'self' https://cdn.jsdelivr.net; connect-src 'self'; frame-ancestors 'none'; object-src 'none'; base-uri 'self'");
         if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') {
             header("Strict-Transport-Security: max-age=31536000; includeSubDomains");
         }
